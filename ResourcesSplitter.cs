c3c9449c94b6e6a24b08c383b3bca61ea1822983
HowTommy <howtommy.net@gmail.com>
2014-09-17 17:47:15 +0200

    First commit without the Visual Studio solution but with the code in a cs file

---
+++ ResourcesSplitter.cs
@@ -0,0 +1,91 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ResourcesSplitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string refToHowTommy =
+                    "// that resource file was created with a tool: resmanager by howtommy (https://github.com/HowTommy/resmanager)";
+
+                var sb = new StringBuilder();
+
+                var sbLogs = new StringBuilder();
+
+                string resources = File.ReadAllText("resources.lng");
+                var lines = resources.Split(new string[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+                var languages = lines[0].Split(new string[] {"\t"}, StringSplitOptions.None).Skip(1);
+
+                string fileBeginningWithLanguages =
+                    string.Format("<?php{0}\t{1}{0}\t$defaultLanguage = '{2}';{0}\t$languages = array('{3}');{0}",
+                        Environment.NewLine,
+                        refToHowTommy,
+                        languages.ElementAt(0),
+                        string.Join("', '", languages));
+
+                for (var i = 0; i < languages.Count(); i++)
+                {
+                    sb.Append(fileBeginningWithLanguages);
+
+                    string language = languages.ElementAt(i);
+
+                    sb.Append(string.Format("\t$resources = array(", Environment.NewLine));
+
+                    bool isFirstLine = true;
+
+                    foreach (var line in lines.Skip(1))
+                    {
+                        if (!line.StartsWith("//"))
+                        {
+                            string[] lineParts = line.Split(new string[] {"\t"}, StringSplitOptions.None);
+
+                            if (lineParts.Count() == languages.Count() + 1)
+                            {
+                                if (isFirstLine)
+                                {
+                                    isFirstLine = false;
+                                }
+                                else
+                                {
+                                    sb.Append(",");
+                                }
+
+                                sb.Append(string.Format("{0}\t\t'{1}' => '{2}'", Environment.NewLine, lineParts[0],
+                                    lineParts[i + 1]));
+                            }
+                            else
+                            {
+                                if (0 == i && lineParts.Any())
+                                {
+                                    sbLogs.AppendLine("Error with resource: " + lineParts[0]);
+                                }
+                            }
+                        }
+                    }
+
+                    sb.Append(string.Format("{0}\t);{0}?>", Environment.NewLine));
+
+                    File.WriteAllText("resources_" + language + ".php", sb.ToString());
+                    sb.Clear();
+                }
+                if (!string.IsNullOrEmpty(sbLogs.ToString()))
+                {
+                    Console.WriteLine(sbLogs.ToString());
+                    Console.Read();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured:");
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ resources_fr.php
@@ -0,0 +1,11 @@
+<?php
+	// that resource file was created with a tool: resmanager by howtommy (https://github.com/HowTommy/resmanager)
+	$defaultLanguage = 'en';
+	$languages = array('en', 'fr');
+	$resources = array(
+		'en' => 'English',
+		'fr' => 'Français',
+		'sample_res_1' => 'sample_res_1_text_fr',
+		'sample_res_2' => 'sample_res_2_text_fr'
+	);
+?>
